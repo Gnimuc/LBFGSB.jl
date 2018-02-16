e85119db9766315f6a8670e71cd9fe4a2131aff5
Gnimuc <qiyupei@gmail.com>
2018-02-16 14:36:18 +0800

    Add build script

--- src/LBFGSB.jl
+++ src/LBFGSB.jl
@@ -1,5 +1,20 @@
 module LBFGSB
 
-# package code goes here
+if VERSION >= v"0.7.0-DEV.3382"
+    import Libdl
+end
+
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    error("LibFoo not installed properly, run Pkg.build(\"LibFoo\"), restart Julia and try again")
+end
+include(depsjl_path)
+
+# Module initialization function
+function __init__()
+    # Always check your dependencies from `deps.jl`
+    check_deps()
+end
 
 end # module
